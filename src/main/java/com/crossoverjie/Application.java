604795d45244ec632bf76c29e2a356afdceb32b9
saurabh <43170074+saurabhacellere@users.noreply.github.com>
2023-09-22 17:02:36 +0530

    Update Application.java

--- src/main/java/com/crossoverjie/Application.java
+++ src/main/java/com/crossoverjie/Application.java
@@ -18,6 +18,16 @@ public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
         LOGGER.info("start ok!");
+                System.out.println("test commit");
+        System.out.println("test commit1");
+                System.out.println("test commit");
+        System.out.println("test commit1");
+ System.out.println("test commit1");
+                System.out.println("test commit");
+        System.out.println("test commit1");
+                System.out.println("test commit");
+        System.out.println("test commit1");
+ System.out.println("test commit1");
 
     }
 
